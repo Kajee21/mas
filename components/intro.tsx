2dfb94c9568bf6163eaa807b6487a4c4a09bb8dc
Kajee21 <108038004+Kajee21@users.noreply.github.com>
2023-08-19 16:29:28 +0530

    intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,10 +64,10 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Kajee.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Student</span> and{" "}
+        <span className="font-bold">forntend</span> developer. I enjoy
+        building <span className="italic">sites & Customizing sites.</span>. My focus is{" "}
+        <span className="underline">HTML,CSS,Java and PHP.</span>.
       </motion.h1>
 
       <motion.div
