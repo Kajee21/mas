7d8e048009398c40114118ddea247b32bc89602f
Kajee21 <108038004+Kajee21@users.noreply.github.com>
2023-08-19 15:09:54 +0530

    intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,10 +31,10 @@ export default function Intro() {
             }}
           >
             <Image
-              src="http://kajee.epizy.com/Kajee/download%20(2)"
-              alt="Kajee portrait"
-              width="537"
-              height="537"
+              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              alt="Ricardo portrait"
+              width="192"
+              height="192"
               quality="95"
               priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
